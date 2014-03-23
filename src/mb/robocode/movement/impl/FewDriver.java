e687be1afeca23c6a2ef820ddf8a51ae64999092
Max Barkley <max.barkley@gmail.com>
2014-03-23 12:20:44 -0400

    Add comments describing wacky one-on-one movement calculations.

--- src/mb/robocode/movement/impl/FewDriver.java
+++ src/mb/robocode/movement/impl/FewDriver.java
@@ -46,21 +46,44 @@ public class FewDriver implements MovementDriver {
           .normalize()
           .scale(2 * Rules.MAX_VELOCITY);
 
+      /*
+       * This expression causes movement towards an opponent, around an opponent
+       * depending on distance.
+       * 
+       * The first term is close to 0.0 when we are far away from an opponent
+       * and approaches 90-degrees when close. This causes us to approach an
+       * opponent when far away, but maneuver around when close.
+       * 
+       * The second term is sinusoidal with a large period (relative to the size
+       * of the game board). The effect of this term is that: when far away we
+       * will directly approach our enemy; in mid range we will zig-zag; and in
+       * close range this term will approach zero and the first term will become
+       * dominant.
+       */
       final double angle = Math.PI / 2.0
           * (Math.pow(Math.max(0.0, dist - rawVector.abs()) / dist, 1.5)
           + Math.sin(Math.pow(2 * Math.PI * rawVector.abs() / dist, 2.0)));
 
       if (!isAwayFromEdge(curPos)) {
+        /*
+         * Scale the movement vector if we are close to the edge to accurately
+         * assess whether we are in danger of running into a wall.
+         */
         vector = vector.normalize().scale(buffer);
       }
 
       if (!isAwayFromEdge(curPos.add(vector.rotate(sign * angle)))) {
+        // Maybe we can try turning in the other direction...
         sign = -1.0;
       }
 
       if (isAwayFromEdge(curPos.add(vector.rotate(sign * angle)))) {
         return vector.rotate(sign * angle);
       } else {
+        /*
+         * If we're really close to the edge, just follow our opponent (since he
+         * may never be passed the edge of the wall).
+         */
         return vector;
       }
     } else {
